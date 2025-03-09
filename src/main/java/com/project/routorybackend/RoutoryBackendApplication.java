4267645884320e5ceab8ce54782331f5a6e8a584
윤진 <jduck1024@naver.com>
2025-03-09 16:11:40 +0900

    :wrench: "first commit"

---
+++ src/main/java/com/project/routorybackend/RoutoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.routorybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutoryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutoryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/routorybackend/RoutoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.routorybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutoryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
