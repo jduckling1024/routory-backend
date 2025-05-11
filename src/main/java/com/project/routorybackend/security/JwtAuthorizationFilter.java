f084dc18d6287cab2fe30834d7f53718ab6b7acc
jduckling_1024 <jduck1024@naver.com>
2025-05-11 22:15:55 +0900

    :wrench: shouldNotFilter list -> set으로 변경

--- src/main/java/com/project/routorybackend/security/JwtAuthorizationFilter.java
+++ src/main/java/com/project/routorybackend/security/JwtAuthorizationFilter.java
@@ -5,13 +5,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Set;
 
 /**
  * 인가 필터
@@ -19,10 +20,7 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
-    private final String SIGN_IN_ENDPOINT = "/api/accounts/sign-in";
-    private final String SIGN_UP_ENDPOINT = "/api/accounts/sign-up";
-    private final String REISSUE_ENDPOINT = "/api/tokens/reissue";
-    private final List<String> EXCLUDE_ENDPOINTS = List.of(SIGN_IN_ENDPOINT, SIGN_UP_ENDPOINT, REISSUE_ENDPOINT);
+    private final Set<String> EXCLUDE_ENDPOINTS = Set.of("/api/accounts/sign-in", "/api/accounts/sign-up", "/api/tokens/reissue");
 
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -30,11 +28,11 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws IOException, ServletException {
-        String authorization = request.getHeader("Authorization");
+        String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
 
         if (authorization != null) {
             final String jwtToken = jwtTokenProvider.parseToken(authorization);
-            final Authentication authentication = jwtTokenProvider.authenticate(jwtToken); // 확인
+            final Authentication authentication = jwtTokenProvider.authenticate(jwtToken);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
@@ -43,6 +41,6 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
-        return EXCLUDE_ENDPOINTS.stream().anyMatch(endpoint -> request.getServletPath().startsWith(endpoint));
+        return EXCLUDE_ENDPOINTS.contains(request.getServletPath());
     }
 }
